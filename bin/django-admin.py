57d7e8dff09367294bd91a6640ac8a8ffd03b703
najeeb97khan <najeeb.khan96@gmail.com>
2016-07-13 14:42:22 +0530

    First Commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/najeebkhan/Desktop/summarise/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ summarise/summarise/views.py
@@ -0,0 +1,1 @@
+### Views are specific to the app
